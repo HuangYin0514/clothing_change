d57a681f177787bb4a8a4efce2e058e8a01039ca
HuangYin <1071324110@qq.com>
2026-07-04 08:37:26 +0800

    feat: init

--- backup.sh
+++ backup.sh
@@ -9,9 +9,9 @@ git add . # 2. 添加所有修改
 git commit -m "feat: ${VERSION_NAME}" # 3. 提交到本地（替换提交说明）
 git pull origin main # 4. 拉取远程最新代码
 git push origin main # 5. 推送到远程\
-echo -e "✅ 成功：代码已提交并同步到远程分支！"
+echo "✅ 成功：代码已提交并同步到远程分支！"
 
-echo -e "🎉 所有操作执行完成！"
+echo "🎉 所有操作执行完成！"
 
 
 ####################################
