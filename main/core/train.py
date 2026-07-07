c7e536d75d688c446b83d7587693bc2b20962ab2
HuangYin <1071324110@qq.com>
2026-07-07 08:21:06 +0800

    initial commit

--- main/core/train.py
+++ main/core/train.py
@@ -9,7 +9,6 @@ def train(config, reid_net, train_loader, criterion, optimizer, scheduler, devic
     meter = util.MultiItemAverageMeter()
     for epoch, data in enumerate(tqdm(train_loader)):
         img, pid, camid, clotheid = data
-        # img, pid, camid, clotheid = img.to(device), pid.to(device), camid.to(device), clotheid.to(device)
 
         if config.MODEL.MODULE == "Lucky":
             B, C, H, W = img.size()
@@ -27,7 +26,6 @@ def train(config, reid_net, train_loader, criterion, optimizer, scheduler, devic
             total_loss += global_tri_loss
 
             optimizer.zero_grad()
-            # total_loss.backward()
             accelerator.backward(total_loss)
             optimizer.step()
 
