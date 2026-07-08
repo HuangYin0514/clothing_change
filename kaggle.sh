b9ee349537e2e30432f853a3270d3bf9fadfb77f
HuangYin <1071324110@qq.com>
2026-07-08 11:08:03 +0800

    NUM_WORKERS

--- kaggle.sh
+++ kaggle.sh
@@ -3,6 +3,6 @@
 # ltcc
 ############################################################
 # Lucky / ltcc  
-# accelerate launch --multi_gpu --num_processes 2 main/main.py --config_file "main/config/method.yml" TASK.NOTES=v13 TASK.NAME=Lucky 
+accelerate launch --multi_gpu --num_processes 2 main/main.py --config_file "main/config/method.yml" TASK.NOTES=v13 TASK.NAME=Lucky 
 
-python main/vis_main.py --config_file "main/config/method.yml"  MODEL.TEST.RESUME_TEST_MODEL=115 
+# python main/vis_main.py --config_file "main/config/method.yml"  MODEL.TEST.RESUME_TEST_MODEL=115 
