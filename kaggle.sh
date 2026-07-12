ff427a07b8b98122b775a2d2029e21eab17fdb1b
HuangYin <1071324110@qq.com>
2026-07-12 08:20:19 +0800

    v19

--- kaggle.sh
+++ kaggle.sh
@@ -5,7 +5,7 @@
 # 调试
 accelerate launch --multi_gpu --num_processes 2 main/main.py \
 --config_file "main/config/method.yml" \
-TASK.NOTES=v19 TASK.NAME=B OPTIMIZER.TOTAL_TRAIN_EPOCH=1 TEST.EVAL_EPOCH=1 TEST.SAVE_START_EPOCH=0
+TASK.NOTES=v19 TASK.NAME=B OPTIMIZER.TOTAL_TRAIN_EPOCH=1 TEST.EVAL_EPOCH=1 TEST.SAVE_START_EPOCH=-1
 
 # 基线
 accelerate launch --multi_gpu --num_processes 2 main/main.py \
