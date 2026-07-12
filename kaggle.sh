5a5faae0a4717033d0a8389fc634f14abf408f32
HuangYin <1071324110@qq.com>
2026-07-12 09:36:17 +0800

    v21

--- kaggle.sh
+++ kaggle.sh
@@ -10,7 +10,7 @@ TASK.NOTES=v19 TASK.NAME=B OPTIMIZER.TOTAL_TRAIN_EPOCH=1 TEST.EVAL_EPOCH=1 TEST.
 # 基线
 accelerate launch --multi_gpu --num_processes 2 main/main.py \
 --config_file "main/config/method.yml" \
-TASK.NOTES=v21 TASK.NAME=B 
+TASK.NOTES=v21 TASK.NAME=B_IBN
 
 # 可视化
 python main/vis_main.py --config_file "main/config/method.yml"
