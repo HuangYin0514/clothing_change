f8cde4703c973565acbfa5fd8ddd649b7857f5fa
HuangYin <1071324110@qq.com>
2026-07-12 09:40:31 +0800

    v23

--- kaggle.sh
+++ kaggle.sh
@@ -3,14 +3,14 @@
 # ltcc
 ############################################################
 # 调试
-accelerate launch --multi_gpu --num_processes 2 main/main.py \
---config_file "main/config/method.yml" \
-TASK.NOTES=v22 TASK.NAME=B OPTIMIZER.TOTAL_TRAIN_EPOCH=1 TEST.EVAL_EPOCH=1 TEST.SAVE_START_EPOCH=-1
+# accelerate launch --multi_gpu --num_processes 2 main/main.py \
+# --config_file "main/config/method.yml" \
+# TASK.NOTES=debug TASK.NAME=B OPTIMIZER.TOTAL_TRAIN_EPOCH=1 TEST.EVAL_EPOCH=1 TEST.SAVE_START_EPOCH=-1
 
 # 基线
 accelerate launch --multi_gpu --num_processes 2 main/main.py \
 --config_file "main/config/method.yml" \
-TASK.NOTES=v22 TASK.NAME=B_IBN
+TASK.NOTES=v23 TASK.NAME=B_IBN
 
 # 可视化
 python main/vis_main.py --config_file "main/config/method.yml"
