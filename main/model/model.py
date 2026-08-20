5300c400dbf603394b8ce5335e59b6a4d5cfab8d
HuangYin <1071324110@qq.com>
2026-08-20 08:05:14 +0800

    Bibn

--- main/model/model.py
+++ main/model/model.py
@@ -28,12 +28,6 @@ class Backbone_R50(nn.Module):
         self.layer3 = resnet.layer3  # 6 blocks / 1024, 24, 12
         self.layer4 = resnet.layer4  # 3 blocks / 2048, 24, 12
 
-        self.fd_l2 = FrequencyDecoupleModule(in_channels=512, reduction=4)
-        self.sfla_l2 = SpatialFrequencyLocalAlignment(in_channels=512)
-
-        self.fd_l3 = FrequencyDecoupleModule(in_channels=1024, reduction=4)
-        self.sfla_l3 = SpatialFrequencyLocalAlignment(in_channels=1024)
-
     def forward(self, img):
         out = self.layer0(img)
         res0_featmap = out
@@ -47,8 +41,6 @@ class Backbone_R50(nn.Module):
         out = self.layer2[1](out)
         out = self.layer2[2](out)
         out = self.layer2[3](out)
-        out = self.fd_l2(out)
-        out = self.sfla_l2(out)
         res2_featmap = out
 
         out = self.layer3[0](out)
@@ -57,8 +49,6 @@ class Backbone_R50(nn.Module):
         out = self.layer3[3](out)
         out = self.layer3[4](out)
         out = self.layer3[5](out)
-        out = self.fd_l3(out)
-        out = self.sfla_l3(out)
         res3_featmap = out
 
         out = self.layer4[0](out)
